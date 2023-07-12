d957c0b76c693af82a0730c62d7ce296126756bd
Bogdan Vasilev <bogdanvasilev@MacBook-Pro-Bogdan.local>
2023-07-12 16:59:48 +0300

    remove: useless usestate

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -3,14 +3,11 @@ import ReactPaginate from "react-paginate";
 
 import { RootState } from "../../modules";
 import { animeActions } from "../../modules/anime/slice";
-import { useEffect, useState } from "react";
 
 export const Pagination = () => {
     const dispatch = useDispatch();
 
     const pageCount = useSelector((state: RootState) => state.anime.allPages);
-
-    const [adaptive, setAdaptive] = useState(false);
 
     const handlePageClick = (event: any) => {
         const page = event.selected + 1;
@@ -20,24 +17,6 @@ export const Pagination = () => {
             window.scrollTo(0, 0)
         }, 100)
     };
-
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth > 500) {
-                setAdaptive(!adaptive);
-            } else {
-                setAdaptive(adaptive);
-            }
-        };
-
-        handleResize(); // Check initial screen width
-
-        window.addEventListener("resize", handleResize);
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
 
     return (
         // className="py-10 mx-16"
